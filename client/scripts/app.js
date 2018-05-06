06744d4e486decb0ff43c78f2171424e9a7198f1
rtsuzaki <rtsuzaki10@gmail.com>
2018-05-05 17:49:34 -0700

    "added all features"

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -5,12 +5,12 @@ var app = {
     $('.username').on(app.handleUsernameClick);
     $('#update-btn').click(this.handleRefresh);
     $('.sendMessage').click(this.handleSubmit);
-    $('.updateRoom').click(this.handleRoomSwitch);
+    $('.updateRoom').click(this.handleRoomAdding);
+    
+    $('button').css('cursor', 'pointer') 
   },
   handleRefresh: function() {
     app.fetch();
-  },
-  handleUsernameClick: function() {
   },
   handleSubmit: function() {
     let endOfUrl = window.location.search.split('=')
@@ -21,10 +21,23 @@ var app = {
         };
     app.send(message);
     app.fetch();
+    $('#message').val('')
   
   },
-  handleRoomSwitch: function() {
+  friendList: [],
+  addingFriends: function(friend) {
+    if(!app.friendList.includes($(friend.target).text().split(':')[0])) {
+      app.friendList.push($(friend.target).text().split(':')[0]);
+      console.log(app.friendList)
+      $('#friendCounter').text(app.friendList.length);
+      $('#friendList').text(app.friendList);
+    }
+    
+    // $(friend.target).css({"background-color": "yellow", "font-size": "200%"})
+  },
+  handleRoomAdding: function() {
     app.renderRoom($('#newRoom').val())
+    $('#newRoom').val('')
   },
   send: function(message) {
     $.ajax({
@@ -55,24 +68,33 @@ var app = {
         
         console.log('chatterbox: Message sent');
 
+        let messageArray = [];
         let roomArray = [];
-       
+        
         if ($('#roomSelect').val() === '1') {
-           app.clearMessages();
-           data.results.forEach(function(el) {
-             app.renderMessage(el);
-           });
+          app.clearMessages();
+          data.results.forEach(function(el) {
+            app.renderMessage(el);
+            // if(app.friendList.includes(el.username)){
+            //   $('#chats').first().css({"background-color": "yellow", "font-size": "200%"})
+            // }
+            // if (el.roomname 
+          });
         } else {
-            app.clearMessages();
-            data.results.forEach(function(el) {
-              if (el.roomname === $('#roomSelect').val()){
-                roomArray.push(el);
-              }    
-            });
-            roomArray.forEach(function(el) {
-             app.renderMessage(el);  
-            });
-        } 
+          app.clearMessages();
+          data.results.forEach(function(el) {
+            if (el.roomname === $('#roomSelect').val()){
+              messageArray.push(el);
+            }    
+          });
+          messageArray.forEach(function(el) {
+           app.renderMessage(el);  
+           // if(app.friendList.includes(el.username)){
+           //    $('#chats').first().css({"background-color": "yellow", "font-size": "200%"})
+           // }
+          });
+        }
+        $('.username').click(app.addingFriends);
       },
       error: function (data) {
         // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
@@ -84,14 +106,15 @@ var app = {
     $('#chats').empty();
   },
   renderMessage: function(message) {
-    $('#chats').append('<div class="username">'+message.username+' : '+message.text+'</div>');
+    $('#chats').append('<div class="username" style="cursor:pointer">'+_.escape(message.username)+':'+_.escape(message.text)+'</div>');
     // $('.username').prepend('<div>'+message.username+'</div>');
     
   },
   renderRoom: function(roomName) {
     let nextRoomNumber = Number($( "option" ).last().val())+1;
     $('option').last().after('<option value ='+nextRoomNumber+'>'+roomName+'</option>')
-  }
+  },
+  myFriendList: [],
 }
 
 
